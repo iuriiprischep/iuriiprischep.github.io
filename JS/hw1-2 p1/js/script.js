c96813b443aca937fd917a22158f21f7b7ff3e29
iurii <uvprischep@rambler.ru>
2016-09-15 23:31:09 +0300

    add js hw1-2

---
+++ JS/hw1-2 p1/js/script.js
@@ -0,0 +1,33 @@
+// Возведение в степень
+do {
+var a = prompt ('Введите целое число, которое нужно возвести в степень');
+} while (isNaN(a) || a!=Math.floor(a))
+
+do {
+var n = prompt ('Введите степень (целое число)');
+} while (isNaN(n) || n!=Math.floor(n))
+
+console.log('a=' + a);
+console.log('n=' + n);
+
+pow(a, n);
+
+function pow(a, n) {
+	var result = a;
+	if (n==0 && a!==0) {
+    	result = 1;
+	} else if (a==0) {
+    	result = 0;
+	} else if (n>0) {
+		for (var i = 1; i < n; i++){
+    	  result = result*a;
+    	}
+	} else {
+		result = 1;
+		for (var i = 0; i < -n; i++){
+    	  result /= a;
+    	}
+	}
+    console.log('Результат ' + result );
+	alert('Результат = ' + result);
+}
---
+++ JS/hw1-2 p2/js/script.js
@@ -0,0 +1,19 @@
+// Авторизация
+var arr = [];
+for (var i = 0; i < 5; i++) {
+  arr[i] = prompt ('Введите имя ' + (i+1) );
+}
+
+var userName = prompt('Введите имя пользователя');
+
+console.log(arr);
+console.log(userName);
+
+while (i > -1) {
+	i = i - 1;
+	if (arr[i] != userName) continue;
+	alert(arr[i] + ' Вы успешно вошли');
+	break;
+}
+
+if (i == -1) alert('Ошибка! Имя не найдено.');
