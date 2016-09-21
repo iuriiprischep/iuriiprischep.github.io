6090260ce68cdbc601a32b449a124c3d920c6a46
iurii <uvprischep@rambler.ru>
2016-09-22 01:10:50 +0300

    add js hw1-2 hw3-4

---
+++ JS/hw3-4/js/script.js
@@ -0,0 +1,87 @@
+// HW 3-4
+'use strict';
+(function() {
+
+var homeWork = {
+
+	addTitle: function(titleText, titleClass) {
+		var title = document.createElement('h3');
+		title.innerHTML = titleText;
+		title.className = titleClass;
+		document.body.appendChild(title);
+	},
+
+	addUl: function(ulClass, parent) {
+		var ul = document.createElement('ul');
+		ul.classList.add(ulClass);
+		ul.setAttribute('id', 'ul' + x);
+		parent.appendChild(ul);
+		return ul;
+	},
+
+	addLi: function(liClass, liText, parent) {
+		var li = document.createElement('li');
+		li.classList.add(liClass);
+		li.setAttribute('id', 'li' + x + "" + y);
+		li.innerHTML = liText;
+		parent.appendChild(li);
+		return li;
+	},
+
+	addInput: function(inputClass, parent) {
+		var input = document.createElement('input');
+		input.setAttribute('type', 'checkbox');
+		input.setAttribute('id', x + "" + y);
+		input.classList.add(inputClass);
+		parent.appendChild(input);
+		return input;
+	},
+
+	addLabel: function(labelClass, parent) {
+		var label = document.createElement('label');
+		label.classList.add(labelClass);
+		label.setAttribute('for', x + "" + y);
+		label.innerHTML = 'Вариант ответа №' + y;
+		parent.appendChild(label);
+		return label;
+	},
+
+	addButton: function(buttonClass) {
+		var button = document.createElement('button');
+		button.classList.add(buttonClass);
+		button.innerHTML = 'Проверить мои результаты';
+		document.body.appendChild(button);
+		return button;
+	}
+
+}
+
+var x = 0;
+var y = 0;
+
+homeWork.addTitle('Тест по программированию', 'titleClass');
+
+homeWork.addUl('ulClassQuest', document.body);
+
+for (var x = 1; x < 4; x++) {
+	var textQuest = x + '.' + 'Вопрос №' + x;
+	var parentLi = document.querySelector('.ulClassQuest');
+	homeWork.addLi('liClassQuest', textQuest, parentLi);
+
+	var parentUl = document.getElementById('li' + x + "" + y);
+	homeWork.addUl('ulClassAnswer', parentUl);
+
+	var parentLi = document.getElementById('ul' + x);
+
+	for (var y = 1; y < 4; y++) {
+		homeWork.addLi('liClassAnswer', " ", parentLi);
+
+		var parentInput = document.getElementById('li' + x + y);
+		homeWork.addInput('inputClass', parentInput);
+		homeWork.addLabel('labelClass', parentInput);
+	};
+};
+
+homeWork.addButton('buttonClass');
+
+})();
