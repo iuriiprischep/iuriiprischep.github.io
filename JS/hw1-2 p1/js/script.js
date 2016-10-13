02c1e02e183bfcb149d2cf6d85a9cbb6a9fe23d6
iurii <uvprischep@rambler.ru>
2016-10-13 22:19:34 +0300

    edit js hw1-2 hw3-4

--- JS/hw1-2 p1/js/script.js
+++ JS/hw1-2 p1/js/script.js
@@ -1,36 +1,49 @@
 // Возведение в степень
 'use strict';
 (function () {
-do {
-var a = prompt ('Введите целое число, которое нужно возвести в степень');
-} while (isNaN(a) || a!=Math.floor(a))
 
-do {
-var n = prompt ('Введите степень (целое число)');
-} while (isNaN(n) || n!=Math.floor(n))
+	var a;
+	var n;
 
-console.log('a=' + a);
-console.log('n=' + n);
+	enterNumbers();
 
-pow(a, n);
+	involution();
 
-function pow(a, n) {
-	var result = a;
-	if (n==0 && a!==0) {
-    	result = 1;
-	} else if (a==0) {
-    	result = 0;
-	} else if (n>0) {
-		for (var i = 1; i < n; i++){
-    	  result = result*a;
-    	}
-	} else {
-		result = 1;
-		for (var i = 0; i < -n; i++){
-    	  result /= a;
-    	}
-	}
-    console.log('Результат ' + result );
-	alert('Результат = ' + result);
-};
+	function enterNumbers() {
+		do {
+			a = prompt ('Введите целое число, которое нужно возвести в степень');
+		} while (isNaN(a) || a != Math.floor(a))
+	
+		do {
+			n = prompt ('Введите степень (целое число)');
+		} while (isNaN(n) || n != Math.floor(n))
+	
+		console.log('a=' + a);
+		console.log('n=' + n);
+	};
+
+	function involution() {
+		var result = a;
+		if (n == 0 && a !== 0) {
+	    	result = 1;
+		};
+
+		if (a == 0) {
+	    	result = 0;
+		};
+
+		if (n > 0) {
+			for (var i = 1; i < n; i++){
+	    	  result = result*a;
+	    	}
+		} else {
+			result = 1;
+			for (var i = 0; i < -n; i++){
+	    	  result /= a;
+	    	}
+		};
+
+	    console.log('Результат ' + result );
+		alert('Результат = ' + result);
+	};
 })();
--- JS/hw1-2 p2/js/script.js
+++ JS/hw1-2 p2/js/script.js
@@ -1,24 +1,35 @@
 // Авторизация
 'use strict';
 (function() {
-var arr = [];
-for (var i = 0; i < 5; i++) {
-  arr[i] = prompt ('Введите имя ' + (i+1) );
-}
+	var arr = [];
+	var i;
+	enterNames();
+	var userName = prompt('Введите имя пользователя');
+	console.log(arr);
+	console.log(userName);
+	authorization();
 
-var userName = prompt('Введите имя пользователя');
+	function enterNames() {
+		for (i = 0; i < 5; i++) {
+		  arr[i] = prompt ('Введите имя ' + (i+1) );
+		};
+	};
 
-console.log(arr);
-console.log(userName);
 
-if (userName == null || userName == "") i = -1;
+	function authorization() {
+		if (userName == null || userName == "") {
+			i = -1;
+		};
 
-while (i > -1) {
-	i = i - 1;
-	if (arr[i] != userName) continue;
-	alert(arr[i] + ' Вы успешно вошли');
-	break;
-}
+		while (i > -1) {
+			i = i - 1;
+			if (arr[i] != userName) continue;
+			alert(arr[i] + ' Вы успешно вошли');
+			break;
+		};
 
-if (i == -1) alert('Ошибка! Имя не найдено.');
+		if (i == -1) {
+			alert('Ошибка! Имя не найдено.');
+		};
+	};
 })();
